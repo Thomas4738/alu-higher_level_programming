21c04cd0a8368e2d7905b61ba095436d6bcffa41
Cydney Thomas <c.thomas@alustudent.com>
2024-06-29 13:51:06 -0700

    8-simple_delete.py

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -4,6 +4,6 @@ def simple_delete(a_dictionary, key=""):
     if a_dictionary:
         try:
             del a_dictionary[key]
-         except KeyError:
+        except KeyError:
                 pass
-            return a_dictionary        
+        return a_dictionary        
